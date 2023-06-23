0f159f432cd29ae166007c8c297f16debbad623c
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-06-22 22:08:04 -0300

    POST - tweets

--- src/app.js
+++ src/app.js
@@ -10,11 +10,28 @@ app.use(express.json())
 app.use(cors())
 
 app.post("/sign-up", (req, res) => {
-    const { username, avatar} = req.body
+    const { username, avatar } = req.body
     const newUser = {
         username,
         avatar
     }
+    users.push(newUser)
+    res.send("OK")
+})
+
+app.post("./tweets", (req, res)=>{
+    const { username, tweet } = req.body
+    const found = users.find(()=>{
+        user => user.username === username
+    })
+    if(!found){
+        return res.send("UNAUTHORIZED")
+    }
+    const newTweet = {
+        username,
+        tweet
+    }
+    tweets.push(newTweet)
     res.send("OK")
 })
 
